e75c7ee108286f73144769a927763faf74a5b144
umurtrky <umur.turkay@gmail.com>
2016-12-04 18:26:25 +0300

    comment

--- HealthTracker/HealthTracker/src/main/java/com/swe/springmvc/configuration/AppConfig.java
+++ HealthTracker/HealthTracker/src/main/java/com/swe/springmvc/configuration/AppConfig.java
@@ -28,9 +28,6 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     RoleToUserProfileConverter roleToUserProfileConverter;
      
  
-    /**
-     * Configure ViewResolvers to deliver preferred views.
-     */
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
  
@@ -42,17 +39,14 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     }
      
     /**
-     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript.
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
     }
      
-    /**
-     * Configure Converter to be used.
-     * In our example, we need a converter to convert string values[Roles] to UserProfiles in newUser.jsp
-     */
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(roleToUserProfileConverter);
@@ -69,10 +63,6 @@ public class AppConfig extends WebMvcConfigurerAdapter{
         return messageSource;
     }
      
-    /**Optional. It's only required when handling '.' in @PathVariables which otherwise ignore everything after last '.' in @PathVaidables argument.
-     * It's a known bug in Spring [https://jira.spring.io/browse/SPR-6164], still present in Spring 4.1.7.
-     * This is a workaround for this issue.
-     */
     @Override
     public void configurePathMatch(PathMatchConfigurer matcher) {
         matcher.setUseRegisteredSuffixPatternMatch(true);
