ae6cbb750a3853efc9e492594fff27b83a6281a5
umurtrky <umur.turkay@gmail.com>
2016-12-11 10:59:22 +0300

    User model updated

--- HealthTracker/HealthTracker/src/main/java/com/swe/springmvc/model/User.java
+++ HealthTracker/HealthTracker/src/main/java/com/swe/springmvc/model/User.java
@@ -1,6 +1,8 @@
 package com.swe.springmvc.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
  
@@ -18,15 +20,15 @@ import javax.persistence.Table;
 import org.hibernate.validator.constraints.NotEmpty;
  
 @Entity
-@Table(name="APP_USER")
+@Table(name="HT_USER")
 public class User implements Serializable{
  
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
  
     @NotEmpty
-    @Column(name="SSO_ID", unique=true, nullable=false)
-    private String ssoId;
+    @Column(name="USERNAME", unique=true, nullable=false)
+    private String username;
      
     @NotEmpty
     @Column(name="PASSWORD", nullable=false)
@@ -39,14 +41,30 @@ public class User implements Serializable{
     @NotEmpty
     @Column(name="LAST_NAME", nullable=false)
     private String lastName;
+    
+    @Column(name="BIRTHDATE", nullable=false)
+    private Date birthdate;
+    
+    @NotEmpty
+    @Column(name="GENDER", nullable=false)
+    private String gender;
+    
+    @Column(name="HEIGHT", nullable=false)
+    private BigDecimal height;
+    
+    @Column(name="WEIGHT", nullable=false)
+    private BigDecimal weight;
+//    birthdate DATE NOT NULL,
+//    gender VARCHAR(30) NOT NULL,
+//    height DECIMAL(3,2) NOT NULL,
+//    weight DECIMAL(4,1) NOT NULL,
  
     @NotEmpty
     @Column(name="EMAIL", nullable=false)
     private String email;
  
-    @NotEmpty
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "APP_USER_USER_ROLE", 
+    @JoinTable(name = "HT_USER_USER_ROLE", 
              joinColumns = { @JoinColumn(name = "USER_ID") }, 
              inverseJoinColumns = { @JoinColumn(name = "USER_ROLE_ID") })
     private Set<UserRole> userRoles = new HashSet<UserRole>();
@@ -59,12 +77,12 @@ public class User implements Serializable{
         this.id = id;
     }
  
-    public String getSsoId() {
-        return ssoId;
+    public String getUsername() {
+        return username;
     }
  
-    public void setSsoId(String ssoId) {
-        this.ssoId = ssoId;
+    public void setUsername(String username) {
+        this.username = username;
     }
  
     public String getPassword() {
@@ -91,7 +109,39 @@ public class User implements Serializable{
         this.lastName = lastName;
     }
  
-    public String getEmail() {
+    public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public BigDecimal getHeight() {
+		return height;
+	}
+
+	public void setHeight(BigDecimal height) {
+		this.height = height;
+	}
+
+	public BigDecimal getWeight() {
+		return weight;
+	}
+
+	public void setWeight(BigDecimal weight) {
+		this.weight = weight;
+	}
+
+	public String getEmail() {
         return email;
     }
  
@@ -112,7 +162,7 @@ public class User implements Serializable{
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((ssoId == null) ? 0 : ssoId.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
         return result;
     }
  
@@ -130,21 +180,18 @@ public class User implements Serializable{
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (ssoId == null) {
-            if (other.ssoId != null)
+        if (username == null) {
+            if (other.username != null)
                 return false;
-        } else if (!ssoId.equals(other.ssoId))
+        } else if (!username.equals(other.username))
             return false;
         return true;
     }
  
-    /*
-     * DO-NOT-INCLUDE passwords in toString function.
-     * It is done here just for convenience purpose.
-     */
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password
+        return "User [id=" + id + ", username=" + username + ", password=" + password
                 + ", firstName=" + firstName + ", lastName=" + lastName
                 + ", email=" + email + "]";
     }
